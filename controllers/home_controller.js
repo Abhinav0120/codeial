69a7ab1576713ae9dccb9c714dfd3c120abf4dae
Abhinav <ajagekarajagekar@gmail.com>
2023-03-06 11:17:51 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,32 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    })
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Post.find({})
+    // .then((posts) =>{
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });   
+    // });
+
+    // Populate the user of each posts
+    Post.find({}).populate('user').exec()
+    .then((posts) =>{
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
+    });
+
+
 }
