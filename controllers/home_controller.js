52af179fbc903cd3e9fd6f076b12146752e693e3
Abhinav <ajagekarajagekar@gmail.com>
2023-03-07 00:55:23 +0530

    Displayed comments and user of comments using Nested Population

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -20,7 +20,15 @@ module.exports.home = function(req, res){
     // });
 
     // Populate the user of each posts
-    Post.find({}).populate('user').exec()
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec()
     .then((posts) =>{
         return res.render('home',{
             title: "Codeial | Home",
