bb619970b4df7fa3966cc079fa9b6cb9bab86740
Abhinav <ajagekarajagekar@gmail.com>
2023-03-20 14:38:06 +0530

    Playing with APIs to understand it more

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,51 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate({
+        path: 'user',
+        select: '-password'
+    })
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user',
+            select: '-password'
+        }
+    });
+
     return res.json(200, {
         message: "v1 List of posts",
-        posts: []
+        posts: posts
     });
+}
+
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id);
+        //.id means converting the objects _id into string
+        // if(post.user == req.user.id){
+
+            // here remove() method is not working but deleteOne() works
+            post.deleteOne();
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.json(200, {
+                message: "Post and associated comments deleted successfully!"
+            }); 
+
+        // }else{
+        //     req.flash('err', 'You can not delete this post!');
+        //     return res.redirect('back');
+        // }
+    }
+    catch(err){ 
+        console.log('****', err);
+        return res.json(500, {
+            message: "Internal server Error"
+        });
+    }
 }
