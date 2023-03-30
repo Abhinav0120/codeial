c539f02425be6d0aefe2b6837c7c39d6965e8f8b
Abhinav <ajagekarajagekar@gmail.com>
2023-03-30 19:33:15 +0530

    setting up Kue and Using it for Delayed Jobs

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -14,7 +14,7 @@ exports.newComment = (comment) =>{
             console.log('Error in sending mail', err);
             return;
         }
-        console.log('Mesage sent', info);
+        // console.log('Mesage sent', info);
         return;
     });
 }
